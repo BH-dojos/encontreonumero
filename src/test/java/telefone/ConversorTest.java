11fa035285c2df39377af129083be6d6ed0fb66b
Dojo People <dojo@thoughtworks.com>
2016-01-06 20:54:35 -0200

    Refatoracao para melhorar verificacao do teste.

--- src/test/java/telefone/ConversorTest.java
+++ src/test/java/telefone/ConversorTest.java
@@ -1,57 +1,58 @@
 package telefone;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class ConversorTest {
+    Conversor conversor;
+
+    @Before
+    public void setUp() throws Exception {
+        conversor = new Conversor();
+    }
+
+    public void verificaConversao(char recebido, char esperado){
+        char convertido = conversor.converte(recebido);
+        assertThat(convertido, is(esperado));
+    }
+
     @Test
     public void umNaoSeTransforma() throws Exception {
         char um = '1';
-        Conversor conversor = new Conversor();
-        char convertido = conversor.converte(um);
-        assertThat(convertido, is(um));
+        verificaConversao(um, um);
     }
 
     @Test
     public void zeroNaoSeTransforma() throws Exception {
         char zero = '0';
-        Conversor conversor = new Conversor();
-        char convertido = conversor.converte(zero);
-        assertThat(convertido, is(zero));
+        verificaConversao(zero, zero);
     }
 
     @Test
     public void hifenNaoSeTransforma() throws Exception {
         char hifen = '-';
-        Conversor conversor = new Conversor();
-        char convertido = conversor.converte(hifen);
-        assertThat(convertido, is(hifen));
+        verificaConversao(hifen, hifen);
     }
 
     @Test
     public void letraASeTransformaEmDois() throws Exception {
         char a = 'A';
-        Conversor conversor = new Conversor();
-        char convertido = conversor.converte(a);
-        assertThat(convertido, is('2'));
+        verificaConversao(a, '2');
     }
 
     @Test
     public void letraBSeTransformaEmDois() throws Exception {
         char b = 'B';
-        Conversor conversor = new Conversor();
-        char convertido = conversor.converte(b);
-        assertThat(convertido, is('2'));
+        verificaConversao(b, '2');
     }
 
     @Test
     public void letraCSeTransformaEmDois() throws Exception {
         char c = 'C';
-        Conversor conversor = new Conversor();
-        char convertido = conversor.converte(c);
-        assertThat(convertido, is('2'));
+        verificaConversao(c, '2');
     }
 
 }
